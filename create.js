3e0e638f4fef7c92eadeb88283d67202528cf4ae
Jay V <jvelayud@gmail.com>
2018-06-12 15:16:48 -0400

    Update create.js

--- create.js
+++ create.js
@@ -3,7 +3,6 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
-  gibberish.what;
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
